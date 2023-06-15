a70b95530c8c4a252725def8f72fa585d1be8608
Katerrynaa <136275888+Katerrynaa@users.noreply.github.com>
2023-06-15 21:49:28 +0200

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,80 @@
+#task1
+name = "Kate"
+day = "Monday"
+print (f'Good day {name}! {day} is a perfect day to learn some python')
+
+name = "Kate"
+day = "Friday"
+print(f'{name} good day! {day} is a perfect day to learn some python.')
+
+name = "Kate"
+day = "Tuesday"
+b = 'Good day {}! {} is a perfect day to learn some python.'
+print(b.format(name, day))
+
+name = "Superhero"
+day = "Sunday"
+s = 'Good day {}! {} is a perfect day to learn some python.'
+print(s.format(name, day))
+
+#task2
+c = "Kate "
+d = "Friday"
+b = c + d
+print(b)
+
+a = "Kate"
+b = "Monday"
+print(a, b)
+
+a = "Good day Kate!"
+b = "Saturday is a perfect day to learn some python."
+c = a + " " + b 
+print(c)
+
+#task 3
+a = 1
+print(a)
+
+v = 50
+d = 145
+print(v + d)
+
+x = 59 + 80 * 200
+print(x)
+
+c = 60 * 20
+print(c)
+
+a, b, c = 35, 70, 50
+n = a + b + c
+print(n)
+
+a = 20
+x = x + 20
+print(x)
+
+a = 150
+b = 37
+c = a - b
+print(c)
+
+a = 6
+a **= 9
+print(a)
+
+a = 24
+b = 6
+print( a / b)
+
+x = 5
+y = 2
+print(x % y)
+
+print("floor division")
+print(16 // 4, "\tis an int.")
+print(16.0 // 4, "\tis a float.")
+
+print("floor division")
+print(18 // 8, "\tis an int.")
+print(18.0 // 8, "\tis a float.")

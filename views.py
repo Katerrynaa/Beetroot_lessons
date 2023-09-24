0137c11d6270792167381b60fe0847a0c89764c2
Katerrynaa <136275888+Katerrynaa@users.noreply.github.com>
2023-09-24 18:51:40 +0200

    Add files via upload

--- views.py
+++ views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-            
-def profile_user(request):
-    profile = ProfileUser.objects.all()
-    return render(request, 'profile_user.html', {'profile': profile})
 
-def message(request):
-    msg = Message.objects.all()
-    return render(request, 'msg.html', {'msg': msg})
+def hello(request):
+    return HttpResponse("Hello from notes app!")
+
+
+
+
+    
+    
+
+    
+

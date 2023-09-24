41332ee9a4d77b8d9f042e5cb8689ae588337de7
Katerrynaa <136275888+Katerrynaa@users.noreply.github.com>
2023-09-24 18:05:41 +0200

    Add files via upload

---
+++ views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def hello_note(request):
+    return HttpResponse('Hello from Notes app')
+
+    
+
+
+
+
+    
+    
+
+    
+

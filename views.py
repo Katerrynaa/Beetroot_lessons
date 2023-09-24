7fe79fd110945d00d49883f9eacaacb41481f870
Katerrynaa <136275888+Katerrynaa@users.noreply.github.com>
2023-09-24 18:55:39 +0200

    Update views.py

--- views.py
+++ views.py
@@ -4,8 +4,14 @@ from django.http import HttpResponse
 def hello(request):
     return HttpResponse("Hello from notes app!")
 
+# lesson 40
+def profile_user(request):
+    profile = ProfileUser.objects.all()
+    return render(request, 'profile_user.html', {'profile': profile})
 
-
+def message(request):
+    msg = Message.objects.all()
+    return render(request, 'msg.html', {'msg': msg})
 
     
     

78a81bf0eb94cf3d98bc2f5d6ab315960024fe79
Katerrynaa <136275888+Katerrynaa@users.noreply.github.com>
2023-10-07 15:41:14 +0200

    Update consumers.py

--- consumers.py
+++ consumers.py
@@ -7,28 +7,21 @@ logger = logging.getLogger(__name__)
 
 class Chat(AsyncWebsocketConsumer):
     async def connect(self):
-        logger.info("Websocket connected")
         await self.accept()
+        await self.send(text_data=json.dumps({'message': "Websocket connected"}))
 
-async def save_msg(self, message, sender, receiver):
-    try:
-        send_user = ProfileUser.objects.get(id=sender)
-        receive_user = ProfileUser.objects.get(id=receiver)
-        new_msg = Message.objects.create(message=message, send_user=send_user, receive_user=receive_user)
-        new_msg.save()
-    except Exception as a:
-        logger.error(f"Error message: {a}")
+    async def disconnect(self):
+        self.send(text_data=json.dumps({'message': 'Websocket disconnected'}))
 
-async def receive(self, text_data):
+    async def receive(self, text_data):
         data_json = json.loads(text_data)
         message = data_json['message']
         sender = data_json['sender']
         receiver = data_json['receiver']
         name_sender = data_json['name_sender']
-        self.save_msg(message, sender, receiver)
 
         await self.save_msg(message, sender, receiver)
-        
+
         await self.send(text_data=json.dumps(
             {
                 'message': message,
@@ -37,3 +30,12 @@ async def receive(self, text_data):
                 'name_sender': name_sender
             }
         ))
+
+    async def save_msg(self, message, sender, receiver):
+        try:
+            send_user = ProfileUser.objects.get(id=sender)
+            receive_user = ProfileUser.objects.get(id=receiver)
+            new_msg = Message.objects.create(message=message, send_user=send_user, receive_user=receive_user)
+            new_msg.save()
+        except Exception as a:
+            logger.error(f"Error message: {a}")
